da093a00e2723b05f0ec6d705ee23d909b6954cc
VitorCarvalho1 <vitorhugo.carvalho2@yahoo.com.br>
2022-06-16 16:31:43 -0300

    Projeto concluído

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 const  inputs = document.querySelectorAll('.input');
 const button = document.querySelector('.login_button');
 
-function handleChange() {
+function alterarBotao() {
     const [username, password] = inputs;
   
-    if (username.value && password.value.length >= 12) {
+    if (username.value && password.value.length >= 8) {
       button.removeAttribute('disabled');     
     } else {
       button.setAttribute('disabled', '')
     }
   }
-inputs.forEach((input) => input.addEventListener('input', handleChange));
+inputs.forEach((input) => input.addEventListener('input', alterarBotao));
 
 
 
